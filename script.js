455e89dc9e7513e85c996bbcd34128559660a29b
Magdalena Perry <magperry16@gmail.com>
2022-03-04 17:41:08 -0500

    restart debugging

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-// prints current time
-var currentTime = moment().format("H:mm:ss");
-
 var containerEl = document.querySelector(".container");
-var currentDay = document.getElementById("currentDay");
-currentDay.textContent(currentTime);
+var dayNowEl = document.getElementById("currentDay");
+
+// current time variable military time
+var timeNow = moment().format("H: mm: ss");
+// creates text as subheader for current time
+dayNowEl.textContent += timeNow;
+//sets up between 8 am - 8 pm
+
