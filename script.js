d06c85592de0653900ec2d9642eea3f34be60e55
Magdalena Perry <magperry16@gmail.com>
2022-03-04 19:29:52 -0500

    color coded elements

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
 var containerEl = document.querySelector(".container");
 var dayNowEl = document.getElementById("currentDay");
-
+var hourBlock = document.getElementsByClassName(timeBlock);
+var textArea = document.getElementsByClassName(".description");
+var inputValue = textArea.value;
+var saveBtns = document.getElementsByTagName("button");
+var time = (moment().hour());
 // current time variable military time
 var timeNow = moment().format("H: mm: ss");
 // creates text as subheader for current time
 dayNowEl.textContent += timeNow;
+
 //sets up between 8 am - 8 pm
+for (var i = 5; i <= 24; i++) {
+
+    // create html code for div container
+    var divRows = document.createElement("div");
+    var timeBlock = document.createElement("div");
+    var taskName = document.createElement("textarea");
+    var saveBtn = document.createElement("button");
+
+    //add created elements to the code
+    containerEl.appendChild(divRows);
+    divRows.appendChild(timeBlock);
+    divRows.appendChild(taskName);
+    divRows.appendChild(saveBtn);
+
+    // add styling classes
+    divRows.classList.add("row", "time-block");
+    timeBlock.classList.add("col-1", "hour");
+    taskName.classList.add("col-10", "description");
+    saveBtn.classList.add("col-1", "saveBtn");
+
+    // set each div container to a specific hour
+    divRows.dataset.hour = i;
+    timeBlock.textContent = i;
+    
+    // colorcoding the classes
+    if (time > i) {
+        taskName.classList.add("past");
+    } else if (time < i) {
+        taskName.classList.add("future");
+    } else {
+        taskName.classList.add("present");
+    }
+
+    // has to be in for loop or code will not save.
+    taskName.value = JSON.parse(localStorage.getItem(i));
+}
+
+containerEl.addEventListener("click", function (event) {
+    if (event.target.matches(".saveBtn")) {
+        localStorage.setItem(
+            event.target.parentNode.dataset.hour,
+            JSON.stringify(
+                event.target.parentNode.querySelector(".description").value
+            )
+        );
+    }
+
+    
+
+});
 
